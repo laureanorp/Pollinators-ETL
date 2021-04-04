039e367de9401b4e6b29f6aedab6470a1deac363
Laureano Ruiz <laureano.ruiz.perez@gmail.com>
2021-04-04 14:23:42 +0200

    Full refector of tests (round and truncate ms)
    - Add pytest fixture to test file
    - Refactor names and add constants
    - Several TODOs remaining

--- test_ETL.py
+++ test_ETL.py
@@ -1,16 +1,40 @@
+import pandas as pd
+import pytest
 
-from ETL_mockup import *
-from pandas import Timestamp
+from ETL_mockup import round_milliseconds, truncate_milliseconds
 
-# Test round_ms()
-# Sample dataset with timestamps, convert to datetime object
-df = pd.DataFrame({'time': ["01/01/2020 00:00:01.600", "01/01/2020 00:00:02.000", "01/01/2020 00:00:03.499"]})
-df['time'] = pd.to_datetime(df['time'], format="%d/%m/%Y %H:%M:%S.%f")
 
-def test_round_ms():
-    round_milliseconds(df, "time")
-    assert df["time"].tolist() == [Timestamp('2020-01-01 00:00:02'), Timestamp('2020-01-01 00:00:02'), Timestamp('2020-01-01 00:00:03')]
+# TODO add 0.500 ms order 0.501
+# Sample dataset with timestamps, then converted to datetime object
 
-def test_truncate_ms():
-    truncate_milliseconds(df, "time")
-    assert df["time"].tolist() == [Timestamp('2020-01-01 00:00:02'), Timestamp('2020-01-01 00:00:02'), Timestamp('2020-01-01 00:00:03')]
+@pytest.fixture
+def df_round_truncate_ms() -> pd.DataFrame:
+    df = pd.DataFrame({'time': ["01/01/2020 00:00:01.600", "01/01/2020 00:00:02.000", "01/01/2020 00:00:03.499"]})
+    df['time'] = pd.to_datetime(df['time'], format="%d/%m/%Y %H:%M:%S.%f")
+    return df
+
+
+# TODO rename this
+DF_ROUNDED_MS = [pd.Timestamp('2020-01-01 00:00:02'),
+                 pd.Timestamp('2020-01-01 00:00:02'),
+                 pd.Timestamp('2020-01-01 00:00:03')]
+
+DF_TRUNCATED_MS = [pd.Timestamp('2020-01-01 00:00:01'),
+                   pd.Timestamp('2020-01-01 00:00:02'),
+                   pd.Timestamp('2020-01-01 00:00:03')]
+
+
+# TODO add docstring
+def test_round_milliseconds(df_round_truncate_ms: pd.DataFrame):
+    """
+
+    :param df_round_truncate_ms:
+    :return:
+    """
+    round_milliseconds(df_round_truncate_ms, "time")
+    assert df_round_truncate_ms["time"].tolist() == DF_ROUNDED_MS
+
+
+def test_truncate_milliseconds(df_round_truncate_ms):
+    truncate_milliseconds(df_round_truncate_ms, "time")
+    assert df_round_truncate_ms["time"].tolist() == DF_TRUNCATED_MS
